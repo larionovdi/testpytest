d33228da8bfd44c9a90792dc19cdff41cabdfa83
Dmitrii Larionov <dimalarionov2020@gmail.com>
2022-06-19 18:13:55 +0300

    Improved tests url https://gorest.co.in/public/v1/users

--- src/base_classes/response.py
+++ src/base_classes/response.py
@@ -7,7 +7,7 @@ class Response:
 
     def __init__(self, response):
         self.response = response
-        self.response_json = response.json()
+        self.response_json = response.json().get('data')
         self.response_status = response.status_code
 
     def validate(self, schema):
@@ -21,7 +21,11 @@ class Response:
 
     def asert_status_code(self, status_code):
         if isinstance(status_code, list):
-            assert self.response_status in status_code, GlobalErrorMessages.wrong_status_code.value
+            assert self.response_status in status_code, self
         else:
-            assert self.response_status == status_code, GlobalErrorMessages.wrong_status_code.value
+            assert self.response_status == status_code, self
         return self
+
+    def __str__(self):
+        return f"\n Status code : {self.response_status} \n Requested url: {self.response.url} \
+            \n Response body {self.response_json}"
--- tests/test_some.py
+++ tests/test_some.py
@@ -2,18 +2,25 @@
 import requests
 #from jsonschema import validate
 from configuration import SERVICE_URL
-
+from pprint import pprint
 from src.base_classes.response import Response
-#from src.schemas.post import POST_SCHEMA
-from src.pydantic_schemas.post import Post
+from src.schemas.user import User
 
-def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-#    print(response)
-    responses = Response(response)
-    responses.asert_status_code(200).validate(Post)
+#from src.schemas.post import POST_SCHEMA
+#from src.pydantic_schemas.post import Post
+
+
+"""     responses = Response(response)
+    responses.asert_status_code(200).validate(Post) """
 
 #   received_posts = response.json()
+
+def test_getting_users_list():
+    response = requests.get(SERVICE_URL)
+    test_object = Response(response)
+    test_object.asert_status_code(300).validate(User)
+    
+
 
    
 
